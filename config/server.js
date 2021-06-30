ff4d48979cb10f0f43fe537caa6c7b7f7b3e7c6c
TarekArar <gt_arar@esi.dz>
2021-06-30 16:38:10 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e581738fe80ae8ac4116fa3b708aad79'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6d421552-def4-407f-a6ba-999c09c16887'
+};
